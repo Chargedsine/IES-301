e6f76591efa029d6d14f38ae9eafd0e2e6871e70
Guilherme Godoy <111367685+Chargedsine@users.noreply.github.com>
2022-08-16 11:34:29 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-das
+ <!DOCTYPE html>
+<html>
+<head>
+<meta=charset="utf-8">
+  <meta name="viewport" content="width=divice-width, inicial-scale=1">
+  <title>exemplos da aula</title>
+</head>
+<body>
+   <ul>
+     <li><a href="">jogo adivinhe o numero</a></li>
+     <li><a href="">aula 2 atividade 1</a></li>
+  </ul>
+</body>
+</html> 
